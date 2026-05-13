3485e1157e471dbca082c799ed6e627610fb9771
r4e <fgf990939@gmail.com>
2026-05-13 22:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    
 # ----------------- Load .env ----------------- #
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -133,4 +148,5 @@ async def delete_chanz(guild):
     await asyncio.gather(*tasks)
     await ctx.send(f"✅ Visual impact done across {len(all_channels)} channels safely!")
   # ----------------- Run Bot ----------------- #
+keep_alive()
 bot.run(os.getenv("TOKEN"))
